75d3cc8fd5fa2cb0d55f813a3662edd70c57ee98
hani <haaniah2011@icloud.com>
2024-03-18 01:23:48 +0000

    numericised benefits, care_options, wellness_program, seek_help, anonymity columns

---
+++ others.py
@@ -0,0 +1,26 @@
+import pandas as pd
+    
+survey = pd.read_csv('survey.csv')
+
+def replace(x):
+    if x in {"Yes"}:
+        return "1"
+    if x in {"No"}:
+        return "2"
+    if x in {"Don't know", "Not sure"}:
+        return "3"
+
+survey['benefits'] = survey['benefits'].apply(replace)
+survey['wellness_program'] = survey['wellness_program'].apply(replace)
+survey['seek_help'] = survey['seek_help'].apply(replace)
+survey['anonymity'] = survey['anonymity'].apply(replace)
+survey['care_options'] = survey['care_options'].apply(replace)
+
+print(survey['benefits'])
+print(survey['wellness_program'])
+print(survey['seek_help'])
+print(survey['anonymity'])
+print(survey['care_options'])
+
+# commiting the changes to the file
+survey.to_csv('survey.csv', index=False)
