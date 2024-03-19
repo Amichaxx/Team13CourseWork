cc79c0f32f57c234ffe96f863d74d2592d7b0810
Amichaxx <aminamalikachabane@icloud.com>
2024-03-19 17:31:54 +0000

    Deleted duplicate files

--- Clean-Age-Column-Script.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-survey = pd.read_csv('cleaned_gender_csv_file.csv')
-
-survey = survey[(survey['Age'] >= 18) & (survey['Age'] <= 70)]
-
-survey.to_csv('cleaned_age_csv_file.csv', index=False)
