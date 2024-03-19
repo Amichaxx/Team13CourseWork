a9b355b6bb151117bedc0e835028981c394a1a63
Amichaxx <aminamalikachabane@icloud.com>
2024-03-19 17:28:40 +0000

    Added cleaning scripts and cleaned database to a folder

---
+++ CleanDatabase/work_interfere.py
@@ -0,0 +1,21 @@
+import pandas as pd
+    
+survey = pd.read_csv('survey.csv')
+
+# removes empty rows
+survey.dropna(subset=['work_interfere'], inplace=True)
+
+def replace(x):
+    if x in {"Often"}:
+        return "1"
+    if x in {"Sometimes"}:
+        return "2"
+    if x in {"Rarely"}:
+        return "3"
+    if x in {"Never"}:
+        return "4"
+
+survey['work_interfere'] = survey['work_interfere'].apply(replace)
+
+print(survey['work_interfere'])
+survey.to_csv('survey.csv', index=False)
