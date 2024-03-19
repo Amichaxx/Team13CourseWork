9a87b78327bcfbf62bfd39c505b21c7501e660d1
hani <haaniah2011@icloud.com>
2024-03-19 11:28:39 +0000

    no_of_employees cleaned

--- no_of_employees.py
+++ no_of_employees.py
@@ -3,12 +3,13 @@ import pandas as pd
 survey = pd.read_csv('survey.csv')
 # Create a function to categorize company size
 def categorize_company_size(employee_count):
-    if 1 <= employee_count <= 50:
+    if employee_count in {"1-5", "6-25", "26-100"}:
         return "Small"
-    elif 50 < employee_count <= 250:
+    elif employee_count in {"100-500"}:
         return "Medium"
     else:
         return "Large"
+    
 survey['no_employees'] = survey['no_employees'].apply(categorize_company_size)
 
 survey.to_csv('cleaned_csv_file.csv', index=False)
