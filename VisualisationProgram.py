fb543164c5685237e58ba7ae53e8e986e86be81e
hani <haaniah2011@icloud.com>
2024-04-08 05:13:10 +0100

    minor changes like comments

--- VisualisationProgram.py
+++ VisualisationProgram.py
@@ -36,7 +36,7 @@ def DiscussionInCompanies(): #hani q2
     # --- Are employees in larger companies more likely to discuss mental health related issues with co-workers and/or supervisors? ---
     # answer columns: coworkers, supervisor
     
-    # Figure 1: BAR CHARTS
+    # Part 1: BAR CHARTS
     small_1_coworkers = data[(data['no_employees']=='Small') & (data['coworkers']==1)].shape[0] # yes - 118
     small_2_coworkers = data[(data['no_employees']=='Small') & (data['coworkers']==2)].shape[0] # some of them - 360
     small_3_coworkers = data[(data['no_employees']=='Small') & (data['coworkers']==3)].shape[0] # no - 106 
@@ -96,6 +96,7 @@ def DiscussionInCompanies(): #hani q2
     r.set_ylabel('No. of Responses')
     r.set_xticks([x + width for x in x_length], x_labels_bar)
     
+    plt.figure(1)
     plt.legend(title='Company Size')
     plt.tight_layout()
     plt.show()  
@@ -103,7 +104,7 @@ def DiscussionInCompanies(): #hani q2
     # --- And does this have a positive/negative outcome? ---
     # answer columns: mental_health_consequence, phys_health_consequence
     
-    # Figure 2: PIE CHARTS
+    # Part 2: PIE CHARTS
     small_1_mental = data[(data['no_employees']=='Small') & (data['mental_health_consequence']==1)].shape[0] # yes - 139 responses
     small_2_mental = data[(data['no_employees']=='Small') & (data['mental_health_consequence']==2)].shape[0] # no - 234 responses
     small_3_mental = data[(data['no_employees']=='Small') & (data['mental_health_consequence']==3)].shape[0] # maybe - 211 responses
@@ -139,7 +140,8 @@ def DiscussionInCompanies(): #hani q2
     y_labels_l_mental = [large_1_mental, large_2_mental, large_3_mental]
     y_labels_l_phys = [large_1_phys, large_2_phys, large_3_phys]
     
-    # figure 2.1: small
+    # figure 2: small
+    plt.figure(2)
     plt.suptitle("Consquences of Discussing Mental Health Related Issues in a Small Sized Company")
             
     plt.subplot(1, 2, 1)
@@ -153,7 +155,8 @@ def DiscussionInCompanies(): #hani q2
     plt.tight_layout()
     plt.show()
     
-    # figure 2.2: medium
+    # figure 3: medium
+    plt.figure(3)
     plt.suptitle("Consquences of Discussing Mental Health Related Issues in a Medium Sized Company")
     
     plt.subplot(1, 2, 1)
@@ -168,6 +171,7 @@ def DiscussionInCompanies(): #hani q2
     plt.show()
     
     # figure 2.3: large
+    plt.figure(4)
     plt.suptitle("Consquences of Discussing Mental Health Related Issues in a Large Sized Company")
     
     plt.subplot(1, 2, 1)
