7e6b7ad4b0c36e3dff3a9d51d95a5efdaec866c8
Stagnant-Water12 <rida.soomro06@gmail.com>
2024-04-15 12:26:35 +0100

    version 2.0

--- VisualisationProgram.py
+++ VisualisationProgram.py
@@ -368,7 +368,7 @@ def company_culture(): #Haaniah, Amina, Rida
         print("=== Company Culture ===")
         print("Questions:")
         print("[a] Do employees in companies with wellness programs report lower levels of work interference because of their mental health?")
-        print("[b] Does the staff in the company have a good or bad image on mental health?")
+        print("[b] Do the staff in tech companies have a good attitude towards mental health?")
         print("[0] Back to main menu")
 
         choice = input("Enter your option: ").strip().lower()
@@ -403,7 +403,37 @@ def company_culture(): #Haaniah, Amina, Rida
 
         elif choice == 'b':
             print("--- Does the staff in the company have a good or bad image on mental health? ---")
-            # Your method call for the question here
+# Do the staff in tech companies have a good attitude towards mental health?
+# Columns used: coworkers, supervisors, seek_help
+            coworker_1 = data[(data['coworkers'] == 1) & (data['seek_help'] == 1)].shape[0] #yes
+            coworker_2 = data[(data['coworkers'] == 1) & (data['seek_help'] == 2)].shape[0] #no
+            coworker_3 = data[(data['coworkers'] == 1) & (data['seek_help'] == 3)].shape[0] #don't know
+
+            supervisor_1 = data[(data['supervisor'] == 1) & (data['seek_help'] == 1)].shape[0] #yes
+            supervisor_2 = data[(data['supervisor'] == 1) & (data['seek_help'] == 2)].shape[0] #no
+            supervisor_3 = data[(data['supervisor'] == 1) & (data['seek_help'] == 3)].shape[0] #don't know
+
+            x = ['Yes', 'No', 'Dont know']
+            y = [coworker_1, coworker_2, coworker_3]
+            z = [supervisor_1, supervisor_2, supervisor_3]
+
+            fig, (coworkers, supervisor) = plt.subplots(1, 2, sharey=True, figsize=(8,8))
+            coworkers.set_title("Staff seeking help from coworkers")
+            supervisor.set_title("Staff seeking help from supervisors")
+
+            coworkers.bar(x, y, color=['plum','pink','paleturquoise'])
+            supervisor.bar(x, z, color=['lightsalmon', 'bisque', 'palegreen'])
+
+            coworkers.set_xlabel('Frequency of coworkers')
+            coworkers.set_ylabel('Responses')
+            supervisor.set_xlabel('Frequency of supervisors')
+            supervisor.set_ylabel('Responses')
+
+            plt.ylim(bottom = 0)
+
+            plt.legend()
+            plt.show()
+    
         elif choice == '0':
             break
         else:
